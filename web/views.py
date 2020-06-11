dfddbe0388676e567dbceda1bd078dd72396451b
unknown <10830183@dcsh.tp.edu.tw>
2020-06-11 11:41:34 +0800

    新增留言，查看留言，留言列表

---
+++ web/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.views.generic import *
+from django.urls import reverse_lazy
+from .models import Message
+# Create your views here.
+# 留言列表
+class MessageList(ListView):
+    model = Message
+
+class MessageDetail(DetailView):
+        model = Message
+
+
+class MessageCreate(CreateView):
+     model = Message
+     field = ['user','subject','content']
+     success_url = reverse_lazy('msg_list')
+     
+class MessageDelete(DeleteView):
+    model = Message
+    success_url = reverse_lazy('msg_delete')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
